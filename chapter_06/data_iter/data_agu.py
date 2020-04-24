024e17859684be83b892ace4e50867130ea52a8b
‘X.Li <xiang.li09@bianlifeng.com>
2020-04-25 00:58:52 +0800

    add chapter 06

---
+++ chapter_06/data_iter/data_agu.py
@@ -0,0 +1,128 @@
+import cv2
+import numpy as np
+import random
+
+# flip 的 landmarks 查表之前 landmarks 序号。
+flip_landmarks_dict = {
+    0:32,1:31,2:30,3:29,4:28,5:27,6:26,7:25,8:24,9:23,10:22,11:21,12:20,13:19,14:18,15:17,
+    16:16,17:15,18:14,19:13,20:12,21:11,22:10,23:9,24:8,25:7,26:6,27:5,28:4,29:3,30:2,31:1,32:0,
+    33:46,34:45,35:44,36:43,37:42,38:50,39:49,40:48,41:47,
+    46:33,45:34,44:35,43:36,42:37,50:38,49:39,48:40,47:41,
+    60:72,61:71,62:70,63:69,64:68,65:75,66:74,67:73,
+    72:60,71:61,70:62,69:63,68:64,75:65,74:66,73:67,
+    96:97,97:96,
+    51:51,52:52,53:53,54:54,
+    55:59,56:58,57:57,58:56,59:55,
+    76:82,77:81,78:80,79:79,80:78,81:77,82:76,
+    87:83,86:84,85:85,84:86,82:87,
+    88:92,89:91,90:90,91:89,92:88,
+    95:93,94:84,93:95
+    }
+# 非形变处理
+def letterbox(img_,img_size=256,mean_rgb = (128,128,128)):
+
+    shape_ = img_.shape[:2]  # shape = [height, width]
+    ratio = float(img_size) / max(shape_)  # ratio  = old / new
+    new_shape_ = (round(shape_[1] * ratio), round(shape_[0] * ratio))
+    dw_ = (img_size - new_shape_[0]) / 2  # width padding
+    dh_ = (img_size - new_shape_[1]) / 2  # height padding
+    top_, bottom_ = round(dh_ - 0.1), round(dh_ + 0.1)
+    left_, right_ = round(dw_ - 0.1), round(dw_ + 0.1)
+    # resize img
+    img_a = cv2.resize(img_, new_shape_, interpolation=cv2.INTER_LINEAR)
+
+    img_a = cv2.copyMakeBorder(img_a, top_, bottom_, left_, right_, cv2.BORDER_CONSTANT, value=mean_rgb)  # padded square
+
+    return img_a
+
+# def scale_coords(img_size, coords, img0_shape):# image size 转为 原图尺寸
+#     # Rescale x1, y1, x2, y2 from 416 to image size
+#     # print('coords     : ',coords)
+#     # print('img0_shape : ',img0_shape)
+#     gain = float(img_size) / max(img0_shape)  # gain  = old / new
+#     # print('gain       : ',gain)
+#     pad_x = (img_size - img0_shape[1] * gain) / 2  # width padding
+#     pad_y = (img_size - img0_shape[0] * gain) / 2  # height padding
+#     # print('pad_xpad_y : ',pad_x,pad_y)
+#     coords[:, [0, 2]] -= pad_x
+#     coords[:, [1, 3]] -= pad_y
+#     coords[:, :4] /= gain
+#     coords[:, :4] = torch.clamp(coords[:, :4], min=0)# 夹紧区间最小值不为负数
+#     return coords
+
+# 图像旋转
+def face_random_rotate(image , pts,angle ,Eye_Left,Eye_Right,fix_res= True,img_size=(256,256),vis = False):
+    cx,cy = (Eye_Left[0] + Eye_Right[0]) / 2,(Eye_Left[1] + Eye_Right[1]) / 2
+    (h , w) = image.shape[:2]
+    h = h
+    w = w
+    # (cx , cy) = (int(0.5 * w) , int(0.5 * h))
+    M = cv2.getRotationMatrix2D((cx , cy) , angle , 1.0)
+    cos = np.abs(M[0 , 0])
+    sin = np.abs(M[0 , 1])
+
+    # 计算新图像的bounding
+    nW = int((h * sin) + (w * cos))
+    nH = int((h * cos) + (w * sin))
+
+    M[0 , 2] += int(0.5 * nW) - cx
+    M[1 , 2] += int(0.5 * nH) - cy
+
+    img_rot = cv2.warpAffine(image , M , (nW , nH),flags=cv2.INTER_LINEAR)
+    #flags : INTER_LINEAR INTER_CUBIC INTER_NEAREST
+    #borderMode : BORDER_REFLECT BORDER_TRANSPARENT BORDER_REPLICATE CV_BORDER_WRAP BORDER_CONSTANT
+
+    pts_r = []
+    for pt in pts:
+        x = float(pt[0])
+        y = float(pt[1])
+
+        x_r = (x*M[0][0] + y*M[0][1] + M[0][2])
+        y_r = (x*M[1][0] + y*M[1][1] + M[1][2])
+
+        pts_r.append([x_r,y_r])
+
+    x = [pt[0] for pt in pts_r]
+    y = [pt[1] for pt in pts_r]
+
+    x1,y1,x2,y2 = np.min(x),np.min(y),np.max(x),np.max(y)
+
+    expand_w = (x2-x1)*0.15,max((x2-x1)*0.15,30)
+    expand_h = (y2-y1)*0.15,max((y2-y1)*0.15,30)
+
+
+    translation_pixels = 60
+
+    scaling = 0.3
+    x1 += random.randint(-int(max((x2-x1)*scaling,translation_pixels)),int((x2-x1)*0.15))
+    y1 += random.randint(-int(max((y2-y1)*scaling,translation_pixels)),int((y2-y1)*0.15))
+    x2 += random.randint(-int((x2-x1)*0.15),int(max((x2-x1)*scaling,translation_pixels)))
+    y2 += random.randint(-int((y2-y1)*0.15),int(max((y2-y1)*scaling,translation_pixels)))
+
+    x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)
+    x1 = int(max(0,x1))
+    y1 = int(max(0,y1))
+    x2 = int(min(x2,img_rot.shape[1]-1))
+    y2 = int(min(y2,img_rot.shape[0]-1))
+
+
+    crop_rot = img_rot[y1:y2,x1:x2,:]
+
+    crop_pts = []
+    width_crop = float(x2-x1)
+    height_crop = float(y2-y1)
+    for pt in pts_r:
+        x = pt[0]
+        y = pt[1]
+        crop_pts.append([float(x-x1)/width_crop,float(y-y1)/height_crop]) # 归一化
+    if vis:
+        for pt in crop_pts:
+            x = int(pt[0]*width_crop)
+            y = int(pt[1]*height_crop)
+
+            cv2.circle(crop_rot, (int(x),int(y)), 2, (255,0,255),-1)
+
+    if fix_res:
+        crop_rot = letterbox(crop_rot,img_size=img_size[0],mean_rgb = (128,128,128))
+
+    return crop_rot,crop_pts
---
+++ chapter_06/prepare_data/read_faces_datasets.py
@@ -0,0 +1,189 @@
+#-*-coding:utf-8-*-
+# date:2020-04-05
+# Author: xiang li
+import os
+import cv2 # 加载OpenCV库
+import argparse # 加载处理命令行参数库
+import shutil
+import json
+import numpy as np
+import sys
+sys.path.append('./')
+from data_iter.data_agu import *
+import random
+
+def refine_face_bbox(landmarks,img_shape):
+    height,width = img_shape
+
+    x = [float(pt[0]) for pt in landmarks]
+    y = [float(pt[1]) for pt in landmarks]
+
+    x1,y1,x2,y2 = np.min(x),np.min(y),np.max(x),np.max(y)
+
+    expand_w = (x2-x1)
+    expand_h = (y2-y1)
+
+    # x1 -= expand_w*0.1
+    # y1 -= expand_h*0.3
+    # x2 += expand_w*0.1
+    # y2 += expand_h*0.1
+
+    x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)
+
+    x1 = int(max(0,x1))
+    y1 = int(max(0,y1))
+    x2 = int(min(x2,width-1))
+    y2 = int(min(y2,height-1))
+
+    return (x1,y1,x2,y2)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--images_path', type=str, default = './datasets/WFLW_images/',
+        help = 'images_path') # 图片路径
+    parser.add_argument('--train_list', type=str,
+        default = './datasets/WFLW_annotations/list_98pt_rect_attr_train_test/list_98pt_rect_attr_train.txt',
+        help = 'annotations_train_list')# 训练集标注信息
+
+    parser.add_argument('--test_list', type=str,
+        default = './datasets/WFLW_annotations/list_98pt_rect_attr_train_test/list_98pt_rect_attr_test.txt',
+        help = 'annotations_test_list')# 测试集标注信息
+    parser.add_argument('--img_size', type=tuple , default = (256,256),
+        help = 'img_size') # 输入模型图片尺寸
+    parser.add_argument('--fix_res', type=bool , default = False,
+        help = 'fix_resolution') # 输入模型样本图片是否保证图像分辨率的长宽比
+
+    parser.add_argument('--vis', type=bool, default = True,
+        help = 'visualization')# 可视化标志位
+
+    args = parser.parse_args()# 解析添加参数
+    print('----------------------------------')
+
+    unparsed = vars(args) # parse_args()方法的返回值为namespace，用vars()内建函数化为字典
+    for key in unparsed.keys():
+        print('{} : {}'.format(key,unparsed[key]))
+
+    r_ = open(args.train_list,'r')
+    lines = r_.readlines()
+
+    idx = 0
+    for line in lines:
+        # print(line)
+        msg = line.strip().split(' ')
+        idx += 1
+        print('idx-',idx,' : ',len(msg))
+        landmarks = msg[0:196]
+        bbox = msg[196:200]
+        attributes = msg[200:206]
+        img_file = msg[206]
+
+        img = cv2.imread(args.images_path+img_file)
+
+        pts = []
+        for i in range(int(len(landmarks)/2)):
+            x = float(landmarks[i*2+0])
+            y = float(landmarks[i*2+1])
+            pts.append([x,y])
+
+        refine_bbox = refine_face_bbox(pts,(img.shape[0],img.shape[1]))
+
+        img_crop = img.copy()[refine_bbox[1]:refine_bbox[3],refine_bbox[0]:refine_bbox[2],:]
+
+        left_eye = np.average(pts[60:68], axis=0)
+        right_eye = np.average(pts[68:76], axis=0)
+        # print(left_eye,right_eye)
+
+        angle_random = random.randint(-25,25)
+        # print(' angle_random : {}'.format(angle_random))
+        img_rot, crop_pts  = face_random_rotate(img, pts, angle_random, left_eye, right_eye,
+            fix_res = args.fix_res,img_size = args.img_size,vis = args.vis)
+
+        #-----------------------------------
+        if args.vis:
+
+            cv2.circle(img, (int(left_eye[0]),int(left_eye[1])), 5, (255,0,255),-1)
+            cv2.circle(img, (int(right_eye[0]),int(right_eye[1])), 5, (255,0,255),-1)
+
+            cv2.rectangle(img, (int(bbox[0]),int(bbox[1])), (int(bbox[2]),int(bbox[3])), (0,255,0), 4)
+            cv2.rectangle(img, (int(refine_bbox[0]),int(refine_bbox[1])), (int(refine_bbox[2]),int(refine_bbox[3])), (0,155,255), 4)
+
+            for i in range(int(len(landmarks)/2)):
+                x = float(landmarks[i*2+0])
+                y = float(landmarks[i*2+1])
+                cv2.circle(img, (int(x),int(y)), 1, (255,0,0),-1)
+
+            cv2.namedWindow('image',0)
+            cv2.imshow('image',img)
+            cv2.namedWindow('rotate',0)
+            cv2.imshow('rotate',img_rot)
+            cv2.namedWindow('face_crop',0)
+            cv2.imshow('face_crop',img_crop)
+            if cv2.waitKey(1) == 27:
+                break
+
+    if args.vis:
+        cv2.destroyAllWindows()
+
+
+    # unparsed['m'] = 999
+    # fs = open('./make_data_train_test_msg.tr_param',"w",encoding='utf-8')
+    # json.dump(unparsed,fs,ensure_ascii=False,indent = 1)
+    # fs.close()
+    #
+    # mkdir_(args.train_datasets,flag_rm = True)# 创建训练集文件夹
+    # mkdir_(args.test_datasets,flag_rm = True)# 创建测试集文件夹
+    #
+    # imgs_list = open(args.images_list,'r',encoding = 'utf-8').readlines()# 图片列表
+    # print(len(imgs_list))
+    # train_test_split_list = open(args.train_test_split,'r',encoding = 'utf-8').readlines()# 训练和测试集分割列表
+    # print(len(train_test_split_list))
+    # bbox_msg_list = open(args.bbox_msg,'r',encoding = 'utf-8').readlines()# 训练和测试集分割列表
+    # print(len(train_test_split_list))
+    #
+    # train_cnt = 0
+    # test_cnt = 0
+    # img_mean_w = 0.# 统计crop图片 width 均值
+    # img_mean_h = 0.# 统计crop图片 height 均值
+    # for i in range(len(imgs_list)):
+    #     image_id,image_name = imgs_list[i].strip('\n').split(' ')# 获取信息 ：图片id 、图片相对路径
+    #     doc_class = image_name.split('/')[0]# 每一类文件夹
+    #     _,is_train = train_test_split_list[i].strip('\n').split(' ')# 获取信息 ：图片id 、是否是训练图片标志位
+    #     is_train = int(is_train)
+    #     _,x,y,w,h = bbox_msg_list[i].strip('\n').split(' ')# 获取信息 ：图片id 、边界框
+    #     x1,y1,x2,y2 = int(float(x)),int(float(y)),int(float(x) + float(w)),int(float(y) + float(h))
+    #     print('[{}/{}] {} : is_train {} '.format(image_id,len(imgs_list),image_name,is_train))
+    #
+    #     if is_train:# 训练集
+    #         path_s = args.train_datasets
+    #         train_cnt += 1
+    #     else:# 测试集
+    #         path_s = args.test_datasets
+    #         test_cnt += 1
+    #
+    #     mkdir_(path_s + doc_class)# 创建训练测试集的分类目录
+    #
+    #     if args.make_cropdata:# 是否制作 crop 数据集
+    #         img = cv2.imread(args.images_path + image_name)
+    #         cv2.imwrite(path_s +image_name,img[y1:y2,x1:x2,:])
+    #         img_mean_w += (x2-x1)
+    #         img_mean_h += (y2-y1)
+    #         print('w,h : ({},{})'.format(int(img_mean_w/(train_cnt+test_cnt)),int(img_mean_h/(train_cnt+test_cnt))))
+    #     else:# 制作原图数据集
+    #         shutil.copyfile(args.images_path + image_name,path_s +image_name )# 将原图片路径拷贝到相应的训练集和测试集类别文件夹
+    #
+    #     if args.vis:
+    #         img = cv2.imread(args.images_path + image_name)# 读取图片可视化
+    #
+    #         bbox = x1,y1,x2,y2
+    #         plot_box(bbox, img, color=(255,0,0), label='bird')
+    #
+    #         cv2.namedWindow('img',0)#窗口大小可改变
+    #         cv2.imshow('img',img)#显示旋转图片
+    #         cv2.waitKey(1)
+    #
+    # print('train datasets len : {}'.format(train_cnt))
+    # print('test datasets len : {}'.format(test_cnt))
+    # if args.vis:
+    #     cv2.destroyAllWindows()
